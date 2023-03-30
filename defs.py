b83301def73d79eaf1f111537b2bbfb6b4ca31a5
malbe074 <65840246+malbe074@users.noreply.github.com>
2023-03-30 02:09:01 -0400

    add extra colours options and remove web related defs

--- defs.py
+++ defs.py
@@ -8,7 +8,6 @@ This file contains general definitions for the planning.py script
 
 """
 
-
 # filedialog file types
 ft_shapefile = ("Shapefile", "*.shp")
 ft_geo_package = ("GeoPackage ", "*.gpkg")
@@ -25,6 +24,7 @@ ft_all = [ft_any, ft_csv, ft_json, ft_shapefile, ft_geo_package, ft_kml]
 # message strings
 msg_value_error = "Please enter a valid integer."
 msg_value_error_float = "Please enter a valid float value."
+msg_processing = "Processing data"
 SQ = "\u00b2"
 
 # colours
@@ -34,6 +34,14 @@ YELLOW = "\033[1;33m"
 BLUE = "\033[1;34m"
 MAGENTA = "\033[1;35m"
 CYAN = "\033[1;36m"
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKCYAN = '\033[96m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
 RST = "\033[0m"
 
 # attribute names
@@ -43,8 +51,7 @@ ID = "ID"
 NAME = "NAME"
 PUID = "GRID_ID"
 AREA_X = "AREA_X"
+SPECIES = "species"
+PU = "pu"
+AMOUNT = "amount"
 
-# other
-URL_INDEX = "http://127.0.0.1:5000/"
-URL_INPUT = "http://127.0.0.1:5000/input"
-URL_SHUTDOWN = "http://127.0.0.1:5000/shutdown"
